00ef181a7b2a28e854a65fef940be8d6f1421ae2
postforty <75767073+postforty@users.noreply.github.com>
2022-11-09 07:33:47 +0900

    치킨 쿠폰 - 실수를 정수로 바꾸기 위해 "<< 0" 비트 연산자 사용할 수 있음

--- programers_Lv0/치킨 쿠폰_비트 연산자 사용.js
+++ programers_Lv0/치킨 쿠폰_비트 연산자 사용.js
@@ -2,7 +2,7 @@ function solution(chicken) {
   let coupon = chicken;
   var answer = 0;
   while (coupon >= 10) {
-    answer += Math.floor(coupon / 10);
+    answer += Math.floor(coupon / 10); // 실수를 정수로 바꿔야. "<< 0"과 같이 비트 연산자 사용할수도 있음
     coupon = Math.floor((coupon % 10) + coupon / 10);
   }
   return answer;
