5561c71773b5fcf18a16b01a3ef941cb76de158a
postforty <75767073+postforty@users.noreply.github.com>
2023-03-22 20:41:52 +0900

    실패율 파이썬 풀이

--- programers_Lv1/실패율.js
+++ programers_Lv1/실패율.js
@@ -32,4 +32,4 @@ function solution(N, stages) {
   return answer;
 }
 
-console.log(solution(4, [4, 4, 4, 4, 4]));
+console.log(solution(4, [5, 5, 5, 5, 5]));
--- programers_Lv1/실패율.py
+++ programers_Lv1/실패율.py
@@ -3,14 +3,31 @@ def solution(N, stages):
 
     count = len(stages)
 
-    stageDict = {}
+    stage_dict = {}
 
     for i in range(1, N+1):
-        stageDict[i] = 0
+        stage_dict[i] = 0
     
     for i in stages:
-        stageDict[(i)] += 1
+        try:
+           stage_dict[i] += 1
+        except:
+            pass
 
-    return stageDict
+    for i in range(1, N+1):
+        temp = stage_dict[i]
+        try:
+            stage_dict[i] = stage_dict[i] / count
+        except:
+            stage_dict[i] = 0
+        finally:
+            count -= temp
 
-print(solution(4, [4, 4, 4, 4, 4]))
+    dic_to_list = stage_dict.items()
+    result = sorted(dic_to_list, key=lambda x: x[1], reverse=True)
+
+    answer = [i[0] for i in result]
+
+    return answer
+
+print(solution(4, [1, 1, 1, 1, 1]))
