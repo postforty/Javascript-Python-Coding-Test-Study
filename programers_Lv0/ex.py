e41665384c03dda037c37933bfd6bf42ee77e72f
Dandy Code! <postforty@naver.com>
2023-04-07 15:13:49 +0900

    Create ex.py

---
+++ programers_Lv0/ex.py
@@ -0,0 +1,13 @@
+def solution(board):
+    answer = 0
+    return answer
+
+
+board = [
+    [0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0], 
+    [0, 0, 0, 0, 0], 
+    [0, 0, 1, 0, 0], 
+    [0, 0, 0, 0, 0]]
+
+print(solution(board))
