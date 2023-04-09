ec8d02283ebaf4323fd23999207834db811aa4d5
postforty <75767073+postforty@users.noreply.github.com>
2023-04-09 21:50:33 +0900

    [레벨1] 체육복

--- programers_Lv1/체육복.py
+++ programers_Lv1/체육복.py
@@ -1,5 +1,24 @@
 def solution(n, lost, reserve):
     answer = 0
+
+    count = n - len(lost)
+    chance = len(reserve)
+
+    for i in lost:
+        for j in reserve:
+            if j-1 == i and chance:
+                count += 1
+                chance -= 1
+                break
+            elif j+1 == i and chance:
+                count += 1
+                chance -= 1
+                break
+
+    answer = count 
+
     return answer
 
 print(solution(5, [2,4], [1,3,5]))
+print(solution(5, [2,4], [3]))
+print(solution(3, [3], [1]))
