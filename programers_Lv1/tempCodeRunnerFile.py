bb03a0bfdcafd076f4eefa6751c94682f2ea82f7
postforty <75767073+postforty@users.noreply.github.com>
2023-04-09 23:27:32 +0900

    [레벨1] 체육복 - 파이썬 통과

--- programers_Lv1/tempCodeRunnerFile.py
+++ programers_Lv1/tempCodeRunnerFile.py
@@ -1,2 +1,4 @@
-
-print(solution(3, [3], [1]))
+3,5])) # 5
+# print(solution(5, [2,4], [3])) # 4
+# print(solution(3, [3], [1])) # 2
+# print(solution(5, [4,2], [3,5])) # 5
--- programers_Lv1/체육복.py
+++ programers_Lv1/체육복.py
@@ -2,23 +2,32 @@ def solution(n, lost, reserve):
     answer = 0
 
     count = n - len(lost)
-    chance = len(reserve)
+    solted_lost = sorted(lost)
+    solted_lost_remove = sorted(lost) # 순회 중인 리스트의 요소를 제거하면 안되기 때문에 새로운 리스트 생성
+    solted_reserve = sorted(reserve)
 
-    for i in lost:
-        for j in reserve:
-            if j-1 == i and chance:
-                count += 1
-                chance -= 1
-                break
-            elif j+1 == i and chance:
-                count += 1
-                chance -= 1
-                break
+    # 여벌을 가지고 있는데 잃어 버린 경우에 대한 처리를 먼저해야 함.
+    for i in solted_lost:
+        if i in solted_reserve:
+            solted_lost_remove.remove(i)
+            solted_reserve.remove(i)
+            count += 1
+    
+    for i in solted_lost_remove:
+        if i-1 in solted_reserve:
+            solted_reserve.remove(i-1)
+            count += 1
+            continue
+        if i+1 in solted_reserve:
+            solted_reserve.remove(i+1)
+            count += 1
 
-    answer = count 
+    answer = count
 
     return answer
 
-print(solution(5, [2,4], [1,3,5]))
-print(solution(5, [2,4], [3]))
-print(solution(3, [3], [1]))
+print(solution(5, [2,4], [1,3,5])) # 5
+print(solution(5, [2,4], [3])) # 4
+print(solution(3, [3], [1])) # 2
+print(solution(5, [4,2], [3,5])) # 5
+print(solution(4, [2,3], [3,4])) # 3
