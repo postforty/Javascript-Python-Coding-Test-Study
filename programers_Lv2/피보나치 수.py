070db41c9327ddabf897dc7f08987b85ed96765b
Dandy Code! <postforty@naver.com>
2023-04-13 13:26:37 +0900

    [레벨2] 파이썬 풀이

--- programers_Lv2/피보나치 수.py
+++ programers_Lv2/피보나치 수.py
@@ -7,7 +7,7 @@ def solution(n):
         return 1
         
     for i in range(n-1):
-        temp = (answer[0] + answer[1]) % 1234567
+        temp = (answer[0] + answer[1]) % 1234567 # 문제에 "1234567를 나눈 나머지 리턴"으로 명시되어 있음
         answer[0] = answer[1]
         answer[1] = temp
 
