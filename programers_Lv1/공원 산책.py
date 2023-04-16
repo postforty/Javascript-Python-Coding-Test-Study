a07aa181728b1b69225f2e6dfa13e8823978e21d
postforty <75767073+postforty@users.noreply.github.com>
2023-04-17 01:00:21 +0900

    [레벨1] 공원 산책 - 실패 - 개선 요망

--- programers_Lv1/공원 산책.py
+++ programers_Lv1/공원 산책.py
@@ -1,6 +1,5 @@
 def solution(park, routes):
     answer = []
-    start = []
     x_point = []
     y_len = len(park)
     x_len = len(park[0])
@@ -8,57 +7,56 @@ def solution(park, routes):
     for i, v in enumerate(park):
         for j, s in enumerate(v):
             if s == 'S':
-                start = [i, j]
+                answer = [i, j]
             if s == 'X':
                 x_point.append([i, j])
     
-    # ! 장애물 좌표와 동일한 위치 뿐만 아니라 이동 중에 만난 장애물에 대한 처리도 고려해야 함
     for i in routes:
         a, b = i.split()
         b = int(b)
         # 가로 - 우
-        if a == "E" and start[1] + b < x_len:
-            temp = start[:]
+        if a == "E" and answer[1] + b < x_len:
+            temp = answer[:]
+            rollback = False
             for j in range(b):
                 temp[1] += 1
-                if temp not in x_point:
-                    start[1] += 1
-                else:
-                    break
+                if temp in x_point:
+                    rollback = True
+            if rollback == False:
+                answer = temp
         # 가로 - 좌
-        if a == "W" and start[1] - b >= 0:
-            temp = start[:]
+        if a == "W" and answer[1] - b >= 0:
+            temp = answer[:]
+            rollback = False
             for j in range(b):
                 temp[1] -= 1
-                if temp not in x_point:
-                    start[1] -= 1
-                else:
-                    break
+                if temp in x_point:
+                    rollback = True
+            if rollback == False:
+                answer = temp
         # 세로 - 상
-        if a == "N" and start[0] - b >= 0:
-            temp = start[:]
+        if a == "N" and answer[0] - b >= 0:
+            temp = answer[:]
             for j in range(b):
                 temp[0] -= 1
-                if temp not in x_point:
-                    start[0] -= 1
-                else:
-                    break
+                if temp in x_point:
+                    rollback = True
+            if rollback == False:
+                answer = temp
         # 세로 - 하
-        if a == "S" and start[0] + b < y_len:
-            temp = start[:]
+        if a == "S" and answer[0] + b < y_len:
+            temp = answer[:]
+            rollback = False
             for j in range(b):
                 temp[0] += 1
-                # ! 진행 중에 장해물을 만나면 롤백 시켜야 함
-                if temp not in x_point:
-                    start[0] += 1
-                else:
-                    break
-            print(start)
-            
+                if temp in x_point:
+                    rollback = True
+            if rollback == False:
+                answer = temp
     
-    return start
+    return answer
 
 # W - 좌, E - 우, N - 상, S - 하 
-# print(solution(["SOO","OOO","OOO"], ["E 2","S 2","W 1"])) # [2,1]
-# print(solution(["SOO","OXX","OOO"], ["E 2","S 2","W 1"])) # [0,1]
+print(solution(["SOO","OOO","OOO"], ["E 2","S 2","W 1"])) # [2,1]
+print(solution(["SOO","OXX","OOO"], ["E 2","S 2","W 1"])) # [0,1]
 print(solution(["OSO","OOO","OXO","OOO"], ["E 2","S 3","W 1"])) # [0,0]
