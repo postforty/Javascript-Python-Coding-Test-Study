cd05dd03bb52d961960abb14a9566805a7d55ee8
Dandy Code! <postforty@naver.com>
2023-04-25 12:39:21 +0900

    [레벨1] 햄버거 만들기 - 파이썬 풀이

--- programers_Lv1/test.py
+++ programers_Lv1/test.py
@@ -1,23 +1,19 @@
 def solution(ingredient):
     answer = 0
-    lst = ingredient
-    ref = [1, 2, 3, 1]
 
     i = 0
-    while True:
-        if len(lst) < 4:
-            return answer
-        if len(lst)-i < 4:
-            return answer
-        if lst[i:i+4] == ref:
-            del lst[i:i+4]
+    # 시간 초과 예방을 위해 순회를 최소화해야 함
+    while i < len(ingredient)-3:
+        if ingredient[i:i+4] == [1, 2, 3, 1]:
+            del ingredient[i:i+4]
             answer += 1
-            i = 0
+            i -= 3
         else:
             i += 1
+    return answer
             
-# print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1])) # 2
-# print(solution([1, 3, 2, 1, 2, 1, 3, 1, 2])) # 0
-# print(solution([1, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 1])) # 2
-# print(solution([1,1,2,3,1,2,3,1,2,3,1,2,3,1])) # 3
+print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1])) # 2
+print(solution([1, 3, 2, 1, 2, 1, 3, 1, 2])) # 0
+print(solution([1, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 1])) # 2
+print(solution([1,1,2,3,1,2,3,1,2,3,1,2,3,1])) # 3
 print(solution([1,1,2,3,1,1])) # 1
