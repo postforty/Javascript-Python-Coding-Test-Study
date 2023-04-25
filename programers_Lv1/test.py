bb19109b0d8240e3ca88f25a162a8ccd4e736210
Dandy Code! <postforty@naver.com>
2023-04-25 11:32:30 +0900

    Update test.py

--- programers_Lv1/test.py
+++ programers_Lv1/test.py
@@ -1,13 +1,23 @@
 def solution(ingredient):
     answer = 0
+    lst = ingredient
+    ref = [1, 2, 3, 1]
 
-        
-
-    return
-
-
-# print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
-# print(solution([1, 3, 2, 1, 2, 1, 3, 1, 2]))
-# print(solution([1, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 1]))
-# print(solution([1,1,2,3,1,2,3,1,2,3,1,2,3,1]))
-print(solution([1,1,2,3,1,1]))
+    i = 0
+    while True:
+        if len(lst) < 4:
+            return answer
+        if len(lst)-i < 4:
+            return answer
+        if lst[i:i+4] == ref:
+            del lst[i:i+4]
+            answer += 1
+            i = 0
+        else:
+            i += 1
+            
+# print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1])) # 2
+# print(solution([1, 3, 2, 1, 2, 1, 3, 1, 2])) # 0
+# print(solution([1, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 1])) # 2
+# print(solution([1,1,2,3,1,2,3,1,2,3,1,2,3,1])) # 3
+print(solution([1,1,2,3,1,1])) # 1
