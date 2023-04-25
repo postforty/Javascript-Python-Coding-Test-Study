96acccb28d09dcc148e84d7a7dfa828e5da7df59
Dandy Code! <postforty@naver.com>
2023-04-25 12:39:53 +0900

    Update 햄버거 만들기.py

--- programers_Lv1/햄버거 만들기.py
+++ programers_Lv1/햄버거 만들기.py
@@ -1,36 +1,19 @@
 def solution(ingredient):
     answer = 0
-    
-    def func(iter):
-        temp = iter[:]
-        count = 0 
-        for i, v in enumerate(iter):
-            try:
-                if temp[i] == 1:
-                    if temp[i+1] == 2:
-                        if temp[i+2] == 3:
-                            if temp[i+3] == 1:
-                                temp[i] = 0
-                                temp[i+1] = 0
-                                temp[i+2] = 0
-                                temp[i+3] = 0
-                                count += 1
-                                break
-            except:
-                break
-        temp = [i for i in temp if i != 0]
-        return [temp, count]
 
-    lst = ingredient
-    while True:
-        if '1231' in ''.join(map(str, lst)):
-            lst, count = func(lst)
-            answer += count
+    i = 0
+    # 시간 초과 예방을 위해 순회를 최소화해야 함
+    while i < len(ingredient)-3:
+        if ingredient[i:i+4] == [1, 2, 3, 1]:
+            del ingredient[i:i+4]
+            answer += 1
+            i -= 3
         else:
-            return answer
-
-# print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
-# print(solution([1, 3, 2, 1, 2, 1, 3, 1, 2]))
-# print(solution([1, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 1]))
-print(solution([1,1,2,3,1,2,3,1,2,3,1,2,3,1]))
-# print(solution([1,1,2,3,1,1]))
+            i += 1
+    return answer
+            
+print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1])) # 2
+print(solution([1, 3, 2, 1, 2, 1, 3, 1, 2])) # 0
+print(solution([1, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 1])) # 2
+print(solution([1,1,2,3,1,2,3,1,2,3,1,2,3,1])) # 3
+print(solution([1,1,2,3,1,1])) # 1
