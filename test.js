2cc4285e7a06173ae6242c9aa1ea4d53e103ad2a
postforty <75767073+postforty@users.noreply.github.com>
2023-05-07 10:00:49 +0900

    [TEST] enumerate 함수와 유사한 자바스크립트 문법 테스트

--- test.js
+++ test.js
@@ -36,16 +36,32 @@ let arr = ["hello", "one", "even", "never", "now", "world", "draw"];
 // });
 // [ 1, 2, 1, 2, 1, 2, 1]
 
-let num = 1;
-const result = arr.map((el, idx) => {
-  if ((idx + 1) % n === 0) {
-    let temp = num;
-    num++;
-    return temp;
-  } else {
-    return num;
-  }
-});
-// [1, 1, 2, 2, 3, 3, 4]
+// let num = 1;
+// const result = arr.map((el, idx) => {
+//   if ((idx + 1) % n === 0) {
+//     let temp = num;
+//     num++;
+//     return temp;
+//   } else {
+//     return num;
+//   }
+// });
+// // [1, 1, 2, 2, 3, 3, 4]
 
-console.log(result);
+// console.log(result);
+
+// for (const [i, v] of ["a", "b", "c"].entries()) {
+//   console.log(typeof i, typeof v);
+//   console.log(i, v);
+// }
+
+// const object = { a: 1, b: 2, c: 3 };
+
+// for (const property in object) {
+//   console.log(object[property]);
+// }
+
+["a", "b", "c"].forEach((v, i) => {
+  console.log(typeof i, typeof v);
+  console.log(i, v);
+});
