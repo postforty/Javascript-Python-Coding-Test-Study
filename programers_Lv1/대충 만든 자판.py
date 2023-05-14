fe43d41c101c52ae614621683b4b185be432a22a
postforty <75767073+postforty@users.noreply.github.com>
2023-05-14 23:34:35 +0900

    [레벨1] 대충 만든 자판 - 파이썬 풀이 중

--- programers_Lv1/대충 만든 자판.py
+++ programers_Lv1/대충 만든 자판.py
@@ -1,6 +1,16 @@
 def solution(keymap, targets):
-    answer = []
-    return answer
+    t0, t1 = targets
+    k0, k1 = keymap
+
+    def func(target):
+        cnt = 0
+        for i in target:
+            temp = [k0.find(i) + 1, k1.find(i) + 1]
+            cnt += max(temp) if 0 in temp else min(temp)
+        return cnt
+
+    return [func(t0), func(t1)]
 
 
 print(solution(["ABACD", "BCEFD"], ["ABCD", "AABB"]))  # [9, 4]
+print(solution(["AA"], ["B"]))  # [9, 4]
