8ae8f5e82def355670635bf9e0d982c228c86b6d
Dandy Code! <postforty@naver.com>
2023-05-23 14:36:59 +0900

    Update test.py

--- programers_Lv1/test.py
+++ programers_Lv1/test.py
@@ -1,16 +1,5 @@
-def solution(n):
-    ans = 1
-    div_n = n
-    
-    while div_n > 1:
-        if div_n % 2 != 0:
-            div_n = div_n // 2
-            ans += 1
-        else:
-            div_n = div_n / 2
+def solution(wallpaper):
+    answer = []
+    return answer
 
-    return ans
-
-print(solution(5)) # 2
-print(solution(6)) # 2
-print(solution(5000)) # 5
+print(solution([".#...", "..#..", "...#."])) # [0, 1, 3, 4]
