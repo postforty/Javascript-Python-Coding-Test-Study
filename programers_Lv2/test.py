139cfa9cfb9c8a6f46361a857436fa385e92bdfa
Dandy Code! <postforty@naver.com>
2023-06-13 17:15:00 +0900

    Create test.py

---
+++ programers_Lv2/test.py
@@ -0,0 +1,6 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3
+def solution(progresses, speeds):
+    answer = []
+    return answer
+
+print(solution([93, 30, 55], [1, 30, 5])) # [2, 1]
