8912816aa5d8f7b913adb441b048ce3ce2ab489b
Dandy Code! <postforty@naver.com>
2023-06-27 16:52:54 +0900

    [레벨1] 카드 뭉치 - 파이썬, 자바스크립트 풀이

--- programers_Lv1/test.py
+++
@@ -1,6 +0,0 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/159994
-def solution(cards1, cards2, goal):
-    answer = ''
-    return answer
-
-print(solution(["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"])) # "Yes"
