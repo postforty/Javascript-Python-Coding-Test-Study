ddd6069f01e887aed6c1ba963eaa61cf890f6efe
postforty <75767073+postforty@users.noreply.github.com>
2023-07-01 18:20:36 +0900

    Update test.py

--- programers_Lv2/test.py
+++ programers_Lv2/test.py
@@ -1,16 +1,22 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43165
+import itertools
+
+
 def solution(numbers, target):
     answer = 0
+    sum_numbers = sum(numbers)
+    temp_lst = []
 
-    for i, v in enumerate(numbers):
-        temp_sum = sum(numbers)
-        temp_sum -= v * 2
-        if temp_sum < target:
-            continue
-        if temp_sum == target:
+    for i in range(1, len(numbers)):
+        temp_lst.extend(list(itertools.permutations(numbers, i)))
+    print(temp_lst)
+    for t in temp_lst:
+        if sum_numbers - (sum(t) * 2) == target:
+            # print(t)
             answer += 1
 
     return answer
 
 
-print(solution([1, 1, 1, 1, 1], 3))  # 5
+# print(solution([1, 1, 1, 1, 1], 3))  # 5
+print(solution([4, 1, 2, 1], 4))  # 2
