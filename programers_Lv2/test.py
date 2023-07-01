d2dc85ad41a75ca7f8bcc9592cf4f91ff51ddf8b
postforty <75767073+postforty@users.noreply.github.com>
2023-07-01 17:37:37 +0900

    Update test.py

--- programers_Lv2/test.py
+++ programers_Lv2/test.py
@@ -1,7 +1,16 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43165
 def solution(numbers, target):
     answer = 0
+
+    for i, v in enumerate(numbers):
+        temp_sum = sum(numbers)
+        temp_sum -= v * 2
+        if temp_sum < target:
+            continue
+        if temp_sum == target:
+            answer += 1
+
     return answer
 
 
-print(solution([1, 1, 1, 1, 1], 3))
+print(solution([1, 1, 1, 1, 1], 3))  # 5
