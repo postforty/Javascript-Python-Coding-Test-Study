190be9d6280119f7d3829886c453e55e6d90034c
postforty <75767073+postforty@users.noreply.github.com>
2023-07-10 21:42:39 +0900

    [레벨2] [3차] 압축 풀이중

---
+++ programers_Lv2/test.js
@@ -0,0 +1,4 @@
+function solution(msg) {
+  var answer = [];
+  return answer;
+}
---
+++ programers_Lv2/test.py
@@ -0,0 +1,11 @@
+import string
+
+
+def solution(msg):
+    answer = []
+    alpa_list = list(string.ascii_uppercase)
+
+    return answer
+
+
+print(solution("KAKAO"))  # [11, 1, 27, 15]
