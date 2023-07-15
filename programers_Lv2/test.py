3bada5ece7e5cd081bf466e2212fcdd8fa27ccc5
postforty <75767073+postforty@users.noreply.github.com>
2023-07-15 18:55:08 +0900

    Create test.py

---
+++ programers_Lv2/test.py
@@ -0,0 +1,33 @@
+def solution(n, t, m, p):
+    num_list = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+    ]
+
+    new_num_list = num_list[:n]
+
+    result = ""
+    count = 0
+    while count < t:
+        for i in new_num_list:
+            result += i
+            print(result)
+            count += 1
+            result = ""
+
+
+print(solution(10, 20, 2, 1))  # "0111"
