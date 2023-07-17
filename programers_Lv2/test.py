2720cd252b7a7097ba623e0fa1211e833085172b
postforty <75767073+postforty@users.noreply.github.com>
2023-07-17 20:46:55 +0900

    Update test.py

---
+++ programers_Lv2/test.py
@@ -0,0 +1,30 @@
+import string
+
+
+def solution(msg):
+    answer = []
+    alpa_list = {v: i + 1 for i, v in enumerate(list(string.ascii_uppercase))}
+    new_num = 27
+
+    new_msg = msg
+    temp_str = ""
+    print(new_msg[0])
+    while True:
+        if new_msg == "":
+            break
+        if new_msg[0] in alpa_list:
+            temp_str += new_msg[0]
+            new_msg = new_msg[1:]
+            continue
+        if temp_str not in alpa_list:
+            alpa_list[temp_str] = new_num
+            temp_str = ""
+            new_num += 1
+            continue
+        else:
+            answer.append(alpa_list[temp_str])
+
+    return answer
+
+
+print(solution("KAKAO"))  # [11, 1, 27, 15]
