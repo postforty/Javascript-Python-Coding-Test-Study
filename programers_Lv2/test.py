fca6fb5c67e23de5a84e4a7ee9b0df4cda7f3d44
Dandy Code! <postforty@naver.com>
2023-07-21 10:24:28 +0900

    [레벨2] 주차 요금 계산 - 파이썬 풀이 중

---
+++ programers_Lv2/test.py
@@ -0,0 +1,6 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/92341
+def solution(fees, records):
+    answer = []
+    return answer
+
+print(solution([180, 5000, 10, 600], ["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"])) # [14600, 34400, 5000]
