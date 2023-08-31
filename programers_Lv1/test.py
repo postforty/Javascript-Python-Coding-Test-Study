c9557a493977bd2bfd45c47103a92542146fe01b
Dandy Code! <postforty@naver.com>
2023-08-31 17:54:41 +0900

    [ADD] 레벨1

---
+++ programers_Lv1/test.py
@@ -0,0 +1,12 @@
+import math
+
+def solution(n, m, section):
+    answer = 0
+    i = 0
+    increment = 0
+    return answer
+
+# print(solution(8, 4, [2, 3, 6])) # 2
+# print(solution(5, 5, [3])) # 1
+# print(solution(4, 2, [3, 4])) # 1
+print(solution(5, 2, [1, 4, 5])) # 2
