caca49197c5070b4c05b2eebf538cfe8485eab67
Dandy Code! <postforty@naver.com>
2023-09-04 16:55:11 +0900

    [레벨1] 문자열 나누기

--- programers_Lv1/test.py
+++ programers_Lv1/test.py
@@ -1,16 +1,24 @@
 def solution(s):
-    answer = 0
+    answer = []
     count = 1
-    char = ""
-    str
-    for i, v in enumerate(s):
-        if v == char:
-            count += 1
-        else:
+    char = s[0]
+    str_result = char
+    for i, v in enumerate(s[1:]):
+        if count != 0:
+            if char == v:
+                count += 1
+            else:
+                count -= 1
+                char = v
+            str_result += v
+        if count == 0:
+            answer.append(str_result)
             count = 1
-            char = v
+            str_result = v
+        print(count)
 
     return answer
 
 
 print(solution("aaabbaccccabba"))  # 3
+# print(solution("ab"))  # 3
