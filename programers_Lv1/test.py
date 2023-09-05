e71543eb3036e92877d55308f5c473a9694ffad4
postforty <75767073+postforty@users.noreply.github.com>
2023-09-05 23:42:01 +0900

    [레벨1] 기사단원의 무기 풀이 중

--- programers_Lv1/test.py
+++ programers_Lv1/test.py
@@ -2,15 +2,18 @@
 def solution(number, limit, power):
     answer = 0
 
-    for n in range(1, number+1):
+    for n in range(1, number + 1):
         count = 0
-        for m in range(1, n+1):
-            count += 1 if n % m == 0 else 0
-            if count > limit:
-                count = power
-                break
+        for i in range(1, int(n**0.5) + 1):
+            if n % i == 0:
+                count += 1
+                if i != n // i:
+                    count += 1
+        if count > limit:
+            count = power
         answer += count
-    
+
     return answer
+
 
 print(solution(10, 3, 2))  # 21
