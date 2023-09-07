e3268e896a8b93221062ed0c8e2c3c6cbd279315
Dandy Code! <postforty@naver.com>
2023-09-07 13:34:54 +0900

    Update 기사단원의 무기.py

--- programers_Lv1/기사단원의 무기.py
+++ programers_Lv1/기사단원의 무기.py
@@ -18,6 +18,21 @@ import math
 
 #     return answer
 
+# 풀이2 : 성공
+# def solution(number, limit, power):
+#     answer = 0
+
+#     for n in range(1, number + 1):
+#         divisor_set = set()
+#         for i in range(1, int(math.sqrt(n)) + 1):
+#             if n % i == 0:
+#                 divisor_set.add(i)
+#                 divisor_set.add(n//i)
+        
+#         answer += power if len(divisor_set) > limit else len(divisor_set)
+
+#     return answer
+
 def solution(number, limit, power):
     answer = 0
 
