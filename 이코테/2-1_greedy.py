ff63b23b13b1097e21ae98f76e74528a2e6771fa
postforty <75767073+postforty@users.noreply.github.com>
2023-11-08 00:28:45 +0900

    Create 2-1_greedy.py

---
+++ 이코테/2-1_greedy.py
@@ -0,0 +1,21 @@
+# greedy
+# n이 1이 될 때까지 k로-나누어 떨어질 경우-나누거나-나누어 떨어지지 않을 경우-1을 뺌
+
+n, k = 10, 2
+
+result = 0
+
+while True:
+    target = (n // k) * k
+    result += n - target
+    n = target
+    if n < k:
+        break
+    result += 1
+    n //= k
+
+result += n - 1
+print(result)
+
+# https://www.youtube.com/watch?v=2zjoKjt97vQ&list=PLRx0vPvlEmdAghTr5mXQxGpHjWqSz0dgC&index=2
+# 18:56부터
