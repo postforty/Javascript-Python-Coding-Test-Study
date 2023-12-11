b9cd82e8cd8e44f325e0a063abe84505e1a903f0
postforty <75767073+postforty@users.noreply.github.com>
2023-12-11 23:59:35 +0900

    Update test.py

--- programers_Lv0/test.py
+++ programers_Lv0/test.py
@@ -12,8 +12,21 @@ def solution(a, b, c, d):
     temp_list = list(zip(temp_dict.keys(), temp_dict.values()))
     temp_list.sort(key=lambda x: x[1], reverse=True)
 
-    if temp_list[0][1] == 3:
-        return (10 * temp_list[0][0] + temp_list[1][0]) ** 2
+    if len(temp_dict) == 2:
+        if temp_list[0][1] == 3:
+            return (10 * temp_list[0][0] + temp_list[1][0]) ** 2
+
+        if temp_list[0][1] == 2:
+            return (temp_list[0][0] + temp_list[1][0]) * abs(temp_list[0][0] - temp_list[1][0])
+
+    print(temp_list)
+    if len(temp_dict) == 3:
+        return (temp_list[1][0] * temp_list[2][0])
+
+    return min(a, b, c, d)
 
 # print(solution(2, 2, 2, 2))
-print(solution(4, 1, 4, 4))
+# print(solution(4, 1, 4, 4))
+# print(solution(6, 3, 3, 6))
+# print(solution(2, 5, 2, 6))
+print(solution(6, 4, 2, 5))
