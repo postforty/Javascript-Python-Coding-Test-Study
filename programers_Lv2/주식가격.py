5d1144544591822183f217f74e3ceb63098fef41
Dandy Code! <postforty@naver.com>
2025-02-18 11:12:16 +0900

    [레벨2] 주식 가격 - 스택 이용

---
+++ programers_Lv2/주식가격.py
@@ -0,0 +1,36 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42584
+# 효율성 테스트 실패
+# def solution(prices):
+#     answer = []
+
+#     for i in range(len(prices)):
+#         current_price = prices[i]
+#         next_prices = prices[i+1:]
+#         second = 0
+#         for price in next_prices:
+#             if current_price <= price:
+#                 second += 1
+#                 continue
+#             second += 1
+#             break
+#         answer.append(second)
+#     return answer
+
+def solution(prices):
+    answer = [0] * len(prices)
+    stack = []
+
+    for i, price in enumerate(prices):
+        while stack and prices[stack[-1]] > price:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+
+    while stack:
+        j = stack.pop()
+        answer[j] = len(prices) - 1 - j
+
+    return answer
+
+print(solution([1,2,3,2,3]))
+print(solution([4,4,2]))
