f6999f6a58d3f3203c5ea01562ed3690e5f043cd
Dandy Code! <postforty@naver.com>
2025-03-18 13:00:31 +0900

    다리를 지나는 트럭 빅오 표기법 분석

--- programers_Lv2/다리를 지나는 트럭.py
+++ programers_Lv2/다리를 지나는 트럭.py
@@ -14,17 +14,19 @@ def solution(bridge_length, weight, truck_weights):
 
     while truck_weights or current_weight > 0:
         answer += 1
-        current_weight -= bridge.pop(0)
+        current_weight -= bridge.pop(0)  # pop(0)의 시간 복잡도는 O(n)
 
         if truck_weights:
             if current_weight + truck_weights[0] <= weight:
                 truck = truck_weights.pop(0)
-                bridge.append(truck)
+                bridge.append(truck)  # append()의 시간 복잡도는 O(1)
                 current_weight += truck
             else:
                 bridge.append(0)
 
     return answer
+
+    # O(n * k) : n은 트럭의 수 (len(truck_weights)), k는 다리의 길이 (bridge_length)
 
 
 print(solution(2, 10, [7, 4, 5, 6]))  # 8
